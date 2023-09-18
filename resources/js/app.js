e24170209717bb274992d419ced684abd5127af2
ReikTorrek <zangan@bk.ru>
2023-09-18 12:48:43 +0300

    add Vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,7 @@ import './bootstrap';
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuex from 'vuex'
 
+createApp(App).use(Vuex);
 createApp(App).use(router).mount("#app")
