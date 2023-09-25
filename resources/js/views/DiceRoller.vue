cbdf7b4d4b41976716cad9704ea8ba42d81b1aa1
ReikTorrek <zangan@bk.ru>
2023-09-25 22:18:40 +0300

    add more pages

---
+++ resources/js/views/DiceRoller.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script>
+import axios from 'axios'
+axios.defaults.withCredentials = true
+export default {
+    name: "DiceRoller",
+}
+</script>
+
+<style scoped>
+
+</style>
